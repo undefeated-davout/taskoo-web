b2ec9c6e49ea83ddb583d670723b58cc91b7dd40
undefeated-davout <34504518+undefeated-davout@users.noreply.github.com>
2022-08-10 19:20:35 +0900

    add: lint/format

--- next.config.js
+++ next.config.js
@@ -2,6 +2,20 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  compiler: (() => {
+    let compilerConfig = {
+      // styledComponentsの有効化
+      styledComponents: true,
+    };
+    if (process.env.NODE_ENV === "production") {
+      compilerConfig = {
+        ...compilerConfig,
+        // 本番環境ではReact Testing Libraryで使用するdata-testid属性を削除
+        reactRemoveProperties: { properties: ["^data-testid$"] },
+      };
+    }
+    return compilerConfig;
+  })(),
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
