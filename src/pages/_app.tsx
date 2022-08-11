390ef0251e4b7d0cdff0d8ad2b9f586845d3da48
undefeated-davout <34504518+undefeated-davout@users.noreply.github.com>
2022-08-11 12:49:21 +0900

    remove: theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,54 +1,43 @@
 import { AppProps } from 'next/app';
-import Head from 'next/head';
-import { createGlobalStyle, ThemeProvider } from 'styled-components';
-import { theme } from 'themes';
+import { createGlobalStyle } from 'styled-components';
 
 // グローバルのスタイル
 const GlobalStyle = createGlobalStyle`
 html,
-body,
-textarea {
+body {
   padding: 0;
   margin: 0;
   font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
     Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
 }
 
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
 * {
   box-sizing: border-box;
 }
 
-a {
-  cursor: pointer;
-  text-decoration: none;
-  transition: .25s;
-  color: ${theme.colors.black};
-}
-
-ol, ul {
-  list-style: none;
+@media (prefers-color-scheme: dark) {
+  html {
+    color-scheme: dark;
+  }
+  body {
+    color: white;
+    background: black;
+  }
 }
 `;
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <meta key="charset" name="charset" content="utf-8" />
-        <meta
-          key="viewport"
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=5"
-        />
-        <meta property="og:locale" content="ja_JP" />
-        <meta property="og:type" content="website" />
-      </Head>
       <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <Component {...pageProps} />
     </>
   );
-};
+}
 
 export default MyApp;
